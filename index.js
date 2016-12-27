01d31f4af2e339d03f61eeb06630c32ea62cc1fc
Roman Sharkov <roman.sharkov@qbeon.com>
2016-12-27 01:49:28 +0100

    Update

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ function validateEmail(email) {
 	return re.test(email);
 }
 
-var qbeon = angular.module('qbeon', ['duScroll', 'ngAnimate', 'ngCookies']);
+var qbeon = angular.module('qbeon', ['duScroll', 'ngAnimate', 'ngTouch', 'ngCookies', 'slickCarousel']);
+
+qbeon.run([
+'$rootScope',
+function(
+	$rootScope
+) {
+}])
+
+
 qbeon.controller('MainController', function(
 	$scope,
 	$window,
-	$cookies
+	$cookies,
+	$interval
 ) {
 	window.scrollTo(0,0);
 	$scope.locales = {
 		'en_US': 'English',
-		'de_DE': 'Deutsch'
+		'de_DE': 'Deutsch',
+		'ru_RU': 'Русский'
 	}
 	
 	$scope.locale = $cookies.get('lang');
@@ -40,12 +51,100 @@ qbeon.controller('MainController', function(
 		$scope.modalOpen = false;
 		$cookies.put('lang', locale);
 	}
+
+	$scope.layer = 'apps';
+	$scope.layerAutoplay = true;
+	$scope.layerIntervalDuration = 6000;
+	
+	$scope.toNextLayer = function(stopAutoplay) {
+		if(stopAutoplay) {
+			$scope.layerAutoplay = false;
+		}
+		switch($scope.layer) {
+			case 'apps':
+				$scope.layer = 'qubeos';
+				break;
+			case 'qubeos':
+				$scope.layer = 'middleware';
+				break;
+			case 'middleware':
+				$scope.layer = 'hypervisor';
+				break;
+			case 'hypervisor':
+				$scope.layer = 'machines';
+				break;
+			case 'machines':
+				$scope.layer = 'apps';
+				break;
+		}
+	}
+
+	$scope.toPreviousLayer = function(stopAutoplay) {
+		if(stopAutoplay) {
+			$scope.layerAutoplay = false;
+		}
+		switch($scope.layer) {
+			case 'qubeos':
+				$scope.layer = 'apps';
+				break;
+			case 'middleware':
+				$scope.layer = 'qubeos';
+				break;
+			case 'hypervisor':
+				$scope.layer = 'middleware';
+				break;
+			case 'machines':
+				$scope.layer = 'hypervisor';
+				break;
+			case 'apps':
+				$scope.layer = 'machines';
+				break;
+		}
+	}
+
+	$scope.toLayer = function(layerName, stopAutoplay) {
+		if(stopAutoplay) {
+			$scope.layerAutoplay = false;
+		}
+		$scope.layer = layerName;
+	}
+
+	$scope.layerInterval = $interval(function() {
+		if($scope.layerAutoplay) {
+			$scope.toNextLayer(false);
+		}
+	}, $scope.layerIntervalDuration);
 })
 
 qbeon.controller('ArticleController', function(
 	$scope,
 	$window
 ) {
+	$scope.curAppInSlider = 0;
+	$scope.appSlider = {
+		enabled: true,
+		draggable: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		method: {},
+		event: {
+			afterChange: function (event, slick, currentSlide, nextSlide) {
+				$scope.curAppInSlider = currentSlide;
+			},
+			init: function (event, slick) {
+				slick.slickGoTo($scope.curAppInSlider);
+			},
+		}
+	}
+
+	$scope.goToAppSlide = function(slideNumber) {
+		$scope.appSlider.method.slickGoTo(slideNumber);
+		$scope.appSlider.method.slickPause();
+	}
+
 	$scope.formName = '';
 	$scope.formEmail = '';
 	$scope.formMessage = '';
@@ -72,7 +171,6 @@ qbeon.controller('ArticleController', function(
 			$scope.notificationNameWrong = false;
 		}
 
-
 		//verify email
 		if(!validateEmail($scope.formEmail)) {
 			$scope.notificationEmailWrong = true;
@@ -93,15 +191,23 @@ qbeon.controller('ArticleController', function(
 			var request = new XMLHttpRequest();
 			$scope.notificationSending = true;
 			request.onreadystatechange = function() {
-				$scope.notificationSending = false;
-				if(request.status == 201) {
-					$scope.resetFields();
-					$scope.notificationSuccess = true;
-				} else {
-					$scope.notificationFailure = true;
+				switch(request.readyState) {
+					case 0:
+					case 1:
+					case 2:
+					case 3:
+						break;
+					case 4:
+						$scope.resetFields();
+						$scope.notificationSending = false;
+						$scope.notificationSuccess = true;
+						break;
+					default:
+						$scope.notificationFailure = true;
 				}
 			};
-			request.open('GET', 'contact-inquiry?name=' + $scope.formName + '&email=' + $scope.formEmail + '&message=' + $scope.formMessage);
+			request.open('GET', '/contact-inquiry?name=' + $scope.formName + '&email=' + $scope.formEmail + '&message=' + $scope.formMessage, true);
+			request.send(null);
 		}
 	}
 })
